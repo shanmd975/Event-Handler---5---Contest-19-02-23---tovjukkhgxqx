58c7a3020fb173cd77f37389da81edfd09e600f7
Mohammad Shanoor <107303734+shanmd975@users.noreply.github.com>
2023-02-25 22:00:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-
+ const buttonClick=(event)=>{
+  event.preventDefault();
+console.log("form submitted")
+ }
   return (
     <div id="main">
       <form>
@@ -11,7 +14,7 @@ const App = () => {
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={buttonClick}>Submit</button>
       </form>
     </div>
   )
